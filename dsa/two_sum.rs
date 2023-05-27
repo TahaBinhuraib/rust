eb1db2d6970039cfab02ece277e52b02b087f69f
TahaBinhuraib <taha.huraib@gmail.com>
2023-05-27 21:50:14 +0100

    two_sum_1

--- dsa/two_sum.rs
+++ dsa/two_sum.rs
@@ -1,15 +1,27 @@
-impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = std::collections::HashMap::new();
+use std::collections::HashMap;
 
-        for (i, num) in nums.iter().enumerate() {
-            if let Some(&index) = map.get(num) {
-                return vec![index, i as i32];
-            } else {
-                map.insert(target - num, i as i32);
-            }
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = HashMap::new();
+
+    for (i, num) in nums.iter().enumerate() {
+        if let Some(&index) = map.get(num) {
+            return vec![index, i as i32];
+        } else {
+            map.insert(target - num, i as i32);
         }
-
-        Vec::new()
     }
+
+    Vec::new()
 }
+
+fn main() {
+    // Problem taken from leetcode
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+
+    let result = two_sum(nums, target);
+
+    println!("Indices are: {:?}", result);
+}
+
